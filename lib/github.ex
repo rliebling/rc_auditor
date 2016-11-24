df30e6d6df71684b030593d2665c105ede4a744b
Rich Liebling <rliebling@vendini.com>
2016-11-23 23:56:31 -0800

    Implement Github.pull_requests as a stream
    the intent is that we only need to process the stream as far as the
    oldest Jira/PR it needs to find.  That is, for each Jira ticket key
    it can scan the PRs until it finds a match.  Most often, it won't
    need to scan all PRs. Instead, we'll build a map, and when the map
    doesn't have a match, it will continue scanning the stream and
    populating the map.

--- lib/github.ex
+++ lib/github.ex
@@ -1,16 +1,19 @@
 defmodule RcAuditor.Github do
   use HTTPoison.Base
 
-  def approvals do
-    post "/graphql", approvals_query, [{"Content-Type", "application/json"}]
+  def approvals(repo_owner, repo_name, pr_number) do
+    post "/graphql",
+        approvals_query(repo_owner, repo_name, pr_number),
+        [{"Content-Type", "application/json"}]
   end
-  def approvals_query do
+
+  def approvals_query(repo_owner, repo_name, pr_number) do
     """
     {
-      repository(owner: "Vendini", name: "ct-api") {
+      repository(owner: "#{repo_owner}", name: "#{repo_name}") {
         id
         name
-        pullRequest(number: 277) {
+        pullRequest(number: #{pr_number}) {
           number
           author {
             id
@@ -36,11 +39,87 @@ defmodule RcAuditor.Github do
       }
     }
     """
-    |> to_post_struct
-    |> Poison.encode!
+    |> graphql_query
   end
 
-  defp to_post_struct(query), do: %{"query": query}
+  def pr_cursor(repo_owner, repo_name) do
+    q = """
+        query PRs($cursor :String) {
+          repository(owner: "#{repo_owner}", name: "#{repo_name}") {
+            id
+            name
+            pullRequests(last: 30, before: $cursor) {
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+                startCursor
+              }
+              totalCount
+              edges{
+                node {
+                  number
+                  title
+                  author {
+                    name
+                    login
+                  }
+                  id
+                }
+              }
+            }
+          }
+        }
+        """
+    pr_items = fn page -> page["repository"]["pullRequests"]["edges"] end
+    new_vars_fn = fn 
+      nil, _ -> nil
+      page, _ -> case page["repository"]["pullRequests"]["pageInfo"]["startCursor"] do
+                   nil -> nil
+                   x -> %{cursor: x}
+                 end
+    end
+    cursor = 
+      %{vars: nil,
+        query: q,
+        items_fn: pr_items,
+        new_vars: new_vars_fn 
+      }
+  end
+
+  def pull_requests(repo_owner, repo_name) do
+    Stream.resource(
+                    fn -> fetch_with_cursor(pr_cursor(repo_owner,repo_name)) end,
+                    &process_items/1,
+                    fn _ -> nil end
+                )
+  end
+
+  defp process_items({nil, %{vars: nil}}), do: {:halt, nil}
+  defp process_items({nil, cursor}) do
+    fetch_with_cursor(cursor) |> process_items
+  end
+  defp process_items({items, cursor}) do
+    {items, {nil, cursor}}
+  end
+
+
+  defp fetch_with_cursor(cursor) do
+    page = graphql_query( cursor.query, cursor.vars).body["data"]
+    new_cursor = %{cursor | vars: cursor.new_vars.(page, cursor.vars) }
+    IO.puts "OLD: " <> inspect(cursor)
+    IO.puts "NEW: " <> inspect(new_cursor)
+    IO.puts "NEW: " <> inspect(page["repository"]["pullRequests"]["pageInfo"])
+    { cursor.items_fn.(page), new_cursor }
+  end
+
+  def graphql_query(query, vars \\ nil) do
+    post_body = query |> to_post_struct(vars) |> Poison.encode!
+    post!("/graphql", post_body, [{"Content-Type", "application/json"}])
+  end
+
+  defp to_post_struct(query, nil), do: %{"query": query}
+  defp to_post_struct(query, vars), do: %{"query": query, "variables": vars}
 
 
   ### HTTPoison stuff
