f1f485d1193db088c4270c1bbe4bf56893b89ab8
Rich Liebling <rliebling@vendini.com>
2016-11-27 10:19:03 -0800

    add to_md/1 method to Approval to convert to markdown

--- lib/rc_auditor/approval.ex
+++ lib/rc_auditor/approval.ex
@@ -1,3 +1,13 @@
 defmodule Approval do
   defstruct stage: "", approver: "", approved_at: nil, link: nil
+
+  def to_md([], label), do: "#{label} | Not found | "
+  def to_md(a, label) do
+    a
+    |> Enum.map(fn a->
+      label <> "|" <> a.approver <> "|" <> a.approved_at
+    end)
+    |> Enum.join("\n")
+  end
+
 end
